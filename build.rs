7592677f9deb34b4905df8176eb0160fb069be66
Nathan Gill <nathan.j.gill@outlook.com>
2025-07-11 14:02:05 +0100

    Use the C++ component as a shared library, rather than static

--- build.rs
+++ build.rs
@@ -7,22 +7,9 @@ fn main() {
     let dst = cmake::Config::new(".").out_dir(&out_dir).build();
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=tlockr_qt");
-
-    println!("cargo:rustc-link-lib=Qt6Quick");
-    println!("cargo:rustc-link-lib=Qt6QmlMeta");
-    println!("cargo:rustc-link-lib=Qt6QmlWorkerScript");
-    println!("cargo:rustc-link-lib=Qt6QmlModels");
-    println!("cargo:rustc-link-lib=Qt6Widgets");
-    println!("cargo:rustc-link-lib=Qt6OpenGL");
-    println!("cargo:rustc-link-lib=Qt6Gui");
-    println!("cargo:rustc-link-lib=Qt6Qml");
-    println!("cargo:rustc-link-lib=Qt6Network");
-    println!("cargo:rustc-link-lib=Qt6Core");
+    println!("cargo:rustc-link-lib=dylib=tlockr_qt");
 
     println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-lib=GL");
-    println!("cargo:rustc-link-lib=GLESv2");
 
     println!("cargo:rerun-if-changed=cpp/");
 }
