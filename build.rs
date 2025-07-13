18e90cd2219a0e29fd537cdae1ef8d3fd65ade06
Nathan Gill <nathan.j.gill@outlook.com>
2025-07-13 19:18:00 +0100

    Remove some unused link targets

--- build.rs
+++ build.rs
@@ -18,18 +18,12 @@ fn main() {
     println!("cargo:rustc-link-lib=static=tlockr_qt");
 
     println!("cargo:rustc-link-lib=Qt6Quick");
-    println!("cargo:rustc-link-lib=Qt6QmlMeta");
-    println!("cargo:rustc-link-lib=Qt6QmlWorkerScript");
-    println!("cargo:rustc-link-lib=Qt6QmlModels");
-    println!("cargo:rustc-link-lib=Qt6Widgets");
     println!("cargo:rustc-link-lib=Qt6OpenGL");
     println!("cargo:rustc-link-lib=Qt6Gui");
     println!("cargo:rustc-link-lib=Qt6Qml");
-    println!("cargo:rustc-link-lib=Qt6Network");
     println!("cargo:rustc-link-lib=Qt6Core");
 
     println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-lib=GL");
     println!("cargo:rustc-link-lib=GLESv2");
 
     println!("cargo:rerun-if-changed=cpp/");
