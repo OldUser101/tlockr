de64afa614c25a5f94dc1561ddedee9b753eec22
Nathan Gill <nathan.j.gill@outlook.com>
2025-07-23 21:38:10 +0100

    chore: add licensing and docs to `manager.rs`

--- src/wayland/communication/manager.rs
+++ src/wayland/communication/manager.rs
@@ -1,6 +1,10 @@
-use std::{collections::HashMap, os::fd::RawFd};
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2025, Nathan Gill
 
-use nix::sys::epoll::EpollFlags;
+/*
+    manager.rs:
+        Communication manager structure for creating communication channels, and dispatching events
+*/
 
 use crate::wayland::communication::{
     channel::CommunicationChannel,
@@ -8,13 +12,17 @@ use crate::wayland::communication::{
     handler::EventHandler,
     param::EventParam,
 };
+use nix::sys::epoll::EpollFlags;
+use std::{collections::HashMap, os::fd::RawFd};
 
+/// Structure for creating and managing communication channels, and dispatching events
 pub struct CommunicationManager {
     channels: HashMap<EventType, CommunicationChannel>,
     event_handlers: HashMap<EventType, Box<dyn EventHandler>>,
 }
 
 impl CommunicationManager {
+    /// Create a new `CommunicationManager`
     pub fn new() -> Self {
         Self {
             channels: HashMap::new(),
@@ -22,10 +30,17 @@ impl CommunicationManager {
         }
     }
 
+    /// Get a `&CommunciationChannel` corresponding to an `EventType`
+    ///
+    /// If there is no corresponding channel, `Option::None` will be returned.
     pub fn get_channel(&self, event_type: EventType) -> Option<&CommunicationChannel> {
         self.channels.get(&event_type)
     }
 
+    /// Open a new `CommunciationChannel` for the given `EventType`
+    ///
+    /// The opened channel is stored internally, and can be retrieved with
+    /// the `get_channel` function.
     pub fn create_channel(
         &mut self,
         event_type: EventType,
@@ -35,6 +50,11 @@ impl CommunicationManager {
         Ok(())
     }
 
+    /// Register an event handler with an event
+    ///
+    /// The `handler` object must implement the `EventHandler` trait.
+    /// The event type of the handler is inferred from the `EventHandler::event_type`
+    /// function.
     pub fn register_handler<H>(&mut self, handler: H)
     where
         H: EventHandler + 'static,
@@ -43,6 +63,10 @@ impl CommunicationManager {
         self.event_handlers.insert(event_type, Box::new(handler));
     }
 
+    /// Dispatch an `Event` object to it's relevant handler.
+    ///
+    /// This handler must be registered with `register_handler` before calling
+    /// this function.
     pub fn dispatch_event(&mut self, event: Event) -> Result<(), Box<dyn std::error::Error>> {
         let event_type = event.event_type;
 
@@ -56,6 +80,11 @@ impl CommunicationManager {
         Ok(())
     }
 
+    /// Construct and send an event object over the channel specified by `event_type`
+    ///
+    /// `param_1` and `param_2` must be compatible with `EventType`.
+    ///
+    /// An error is returned if there is no channel corresponding to `event_type`.
     pub fn send_event<P1, P2>(
         &mut self,
         event_type: EventType,
@@ -76,6 +105,9 @@ impl CommunicationManager {
         Ok(())
     }
 
+    /// Read a single `Event` object from the channel specified by `event_type`
+    ///
+    /// An error is returned if there is no channel corresponding to `event_type`.
     pub fn receive_event(
         &mut self,
         event_type: EventType,
@@ -90,6 +122,11 @@ impl CommunicationManager {
         Ok(event)
     }
 
+    /// Get read file descriptors for all pipes stored by the current `CommunicationManager`
+    /// instance.
+    ///
+    /// The returned data is of an `epoll` compatible format, containing the
+    /// file descriptor, `EpollFlags`, and data.
     pub fn get_fds(&self) -> Vec<(RawFd, EpollFlags, u64)> {
         self.channels
             .iter()
@@ -102,6 +139,7 @@ impl CommunicationManager {
             .collect()
     }
 
+    /// Receive and dispatch a single event of type `event_type`.
     pub fn handle_event(
         &mut self,
         event_type: EventType,
