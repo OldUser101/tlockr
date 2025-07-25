6bd1f324bc0b56a6dc81ae62a5136bb70b7d69ad
Nathan Gill <nathan.j.gill@outlook.com>
2025-07-25 22:47:48 +0100

    feat: add C-compatible event serial generator

--- src/wayland/communication/serial.rs
+++ src/wayland/communication/serial.rs
@@ -3,5 +3,9 @@ use std::sync::atomic::{AtomicU32, Ordering};
 static SERIAL_COUNTER: AtomicU32 = AtomicU32::new(1);
 
 pub fn next_serial() -> u32 {
+    unsafe { _next_serial() }
+}
+
+unsafe extern "C" fn _next_serial() -> u32 {
     SERIAL_COUNTER.fetch_add(1, Ordering::SeqCst)
 }
