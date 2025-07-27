bc581d8c1b574d4e09f3bf8168255cc1703e0338
Nathan Gill <nathan.j.gill@outlook.com>
2025-07-27 21:56:30 +0100

    chore: add licensing and docs to `manager.rs`

--- src/wayland/event/manager.rs
+++ src/wayland/event/manager.rs
@@ -1,16 +1,31 @@
-use std::collections::HashMap;
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2025, Nathan Gill
+
+/*
+    manager.rs:
+        Event manager structure and event handler registration.
+*/
 
 use crate::wayland::{
     communication::manager::CommunicationManager,
     event::{event::EventType, handler::EventHandler},
 };
+use std::collections::HashMap;
 
+/// `EventManager` strcuture used for event handling and event loops
+///
+/// This structure is used to delegate events to various event handlers,
+/// that are registered with this function.
 pub struct EventManager {
     pub comm_manager: CommunicationManager,
     pub event_handlers: HashMap<EventType, Box<dyn EventHandler>>,
 }
 
 impl EventManager {
+    /// Create a new event manager, with a `CommunicationManager`
+    ///
+    /// The `CommunicationManager` passed to this function is moved, and is
+    /// used exclusively by this object, and registered event handlers.
     pub fn new(comm_manager: CommunicationManager) -> Self {
         Self {
             comm_manager: comm_manager,
